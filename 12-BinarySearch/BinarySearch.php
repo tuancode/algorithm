5acc3535bb26dda34001ef8cabdb8eaebdb9b347
Tuan Vu <tuan.vu@dirox.net>
2017-07-23 08:38:33 +0700

    BinarySearch: add c++ implemtation

--- 12-BinarySearch/BinarySearch.php
+++ 12-BinarySearch/BinarySearch.php
@@ -1,12 +1,20 @@
 <?php
-function binarySearch(string $needle, array $haystack) {
+
+/**
+ * Search value from a list by applying Binary Search Algorithm.
+ *
+ * @param int needle   The value to search for
+ * @param array haystack The list to search in
+ * @return int Position of needle in list if found, otherwise -1.
+*/
+function binarySearch(int $needle, array $haystack): int
+{
     $low = 0;
     $high = count($haystack) - 1;
 
     while ($low <= $high) {
-        $mid = round(($low + $high) / 2);
+        $mid = intval(($low + $high) / 2);
         $guess = $haystack[$mid];
-
         if ($guess == $needle) {
             return $mid;
         } elseif ($guess < $needle) {
@@ -15,15 +23,16 @@ function binarySearch(string $needle, array $haystack) {
             $high = $mid - 1;
         }
     }
-    return 'none';
+
+    return -1;
 }
 
-$handle = fopen("php://stdin", "r");
-$haystack = explode(' ', trim(fgets($handle)));
-$needle = trim(fgets($handle));
+$num = rtrim(fgets(STDIN));
+$stackInput = explode(' ', rtrim(fgets(STDIN)));
+$haystack = array_slice($stackInput, 0, $num);
+$needle = rtrim(fgets(STDIN));
 
-echo 'result: ' . binarySearch($needle, $haystack);
+$result = binarySearch($needle, $haystack);
+echo 'result: ';
+echo ($result != -1) ? $result : 'none';
 echo "\n";
-
-fclose($handle);
-?>
---
+++ 12-BinarySearch/binary_search.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Search value from a list by applying Binary Search Algorithm.
+ *
+ * @param needle   The value to search for
+ * @param haystack The list to search in
+ * @return Position of needle in list if found, otherwise -1.
+*/
+int binarySearch(int needle, vector<int> haystack) {
+    int low = 0;
+    int high = haystack.size() - 1;
+    int mid, guess;
+    
+    while (low <= high) {
+        mid = (low + high) /2;
+        guess = haystack[mid];
+        if (guess == needle) {
+            return mid;
+        } else if (guess < needle) {
+            low = mid + 1;
+        } else if (guess > needle) {
+            high = mid -1;
+        }
+    }
+    
+    return -1;
+}
+
+int main() {
+    int num, needle, input, result;
+    vector<int> haystack;
+    
+    cin >> num;
+    for (int i = 0; i < num; ++i) {
+        cin >> input;
+        haystack.push_back(input);
+    }
+    cin >> needle;
+    
+    result = binarySearch(needle, haystack);
+    cout << "result: ";
+    if (result != -1) {
+        cout << result;
+    } else {
+        cout << "none";
+    }
+    cout << "\n";
+    
+    return 0;
+}
